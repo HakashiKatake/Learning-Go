8a7b5e1628cf79b704509fbdca70e419dba0e843
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-04-11 22:33:59 +0530

    comments

--- printing-names/main.go
+++ printing-names/main.go
@@ -1,8 +1,9 @@
-package main
+package main //this is the package name should be same as the folder name
 
-import "fmt"
+import "fmt" //this is to import the package fmt, needed to print the output
 
-func main() {
-	fmt.Println("Hi! Saurabh")
-	fmt.Println("Hi! Romulous")
+func main() { //this is the main function, the entry point of the program
+	fmt.Println("Hi! Saurabh")  //the fmt package imports the fmt fuction
+	fmt.Println("Hi! Romulous") //the println function prints the string to the console
+	fmt.Print("Test")           //print function prints the string to the console without a new line
 }
