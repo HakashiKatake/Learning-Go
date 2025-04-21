f835b4482dcf944f52719a351fe786585aed60df
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-04-21 09:54:17 +0530

    methods 2

--- Chapter-2/Methods/main.go
+++ Chapter-2/Methods/main.go
@@ -13,7 +13,42 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+type Car struct {
+	Name string
+	Year int
+}
+
+func (c Car) IsLatest() bool {
+	return c.Year >= 2017
+}
+
+//	func (c Car) UpdateName(name string) {
+//		c.Name = name
+//	}
+//
+// pointer type
+func (c *Car) UpdateName(name string) {
+	c.Name = name
+}
+
+type MyInt int
+
+func (i MyInt) isGreater(value int) bool {
+	return i > MyInt(value)
+}
+
 func main() {
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
+	c := Car{"Tesla", 2021}
+
+	fmt.Println("IsLatest", c.IsLatest())
+
+	c.UpdateName("Tesla Model S")
+	fmt.Println("Updated Name", c.Name)
+
+	i := MyInt(10)
+
+	fmt.Println(i.isGreater(5))
+
 }
